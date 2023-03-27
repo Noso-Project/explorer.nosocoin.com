c17c90b4a4282da11113d48104034a9dbf037e12
gonefishing4422 <119458070+gonefishing4422@users.noreply.github.com>
2023-03-27 15:21:55 -0500

    Update countdown method

--- js/countdown.js
+++ js/countdown.js
@@ -1,25 +1,37 @@
+let countdownTimer;
+
+function startCountdown(countdownTime) {
+  clearInterval(countdownTimer);
+  
+  const now = Date.now();
+  const distance = countdownTime - now;
+  
+  if (distance <= 0) {
+    document.getElementById('countdown-timer').innerHTML = 'Block Created';
+    fetchCountdown();
+    return;
+  }
+  
+  const minutes = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById('countdown-timer').innerHTML = `${minutes}m ${seconds}s`;
+  
+  countdownTimer = setTimeout(() => {
+    startCountdown(countdownTime);
+  }, 1000);
+}
+
 function fetchCountdown() {
   fetch('https://nosostats.com:49443/api/endOfBlockInUnix')
     .then(response => response.text())
     .then(data => {
       const countdownTime = data.trim() * 1000;
-      const now = Date.now();
-      const distance = countdownTime - now;
-      const minutes = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60));
-      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-      document.getElementById('countdown-timer').innerHTML = `${minutes}m ${seconds}s`;
-      if (distance < 0) {
-        clearInterval(countdownTimer);
-        document.getElementById('countdown-timer').innerHTML = 'Block Created';
-        // Call fetchCountdown again to refresh the countdown timer
-        setTimeout(fetchCountdown, 5000);
-      }
+      startCountdown(countdownTime);
     })
     .catch(error => {
       console.error('Error:', error);
     });
 }
 
-// Call fetchCountdown every 1 second
+// Call fetchCountdown to start the countdown
 fetchCountdown();
-setInterval(fetchCountdown, 1000);
