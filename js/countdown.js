874cf284bb2f695a852e92e32349abec29cf3d40
gonefishing4422 <119458070+gonefishing4422@users.noreply.github.com>
2023-03-27 16:01:09 -0500

    Update countdown function
    Wait for 5 seconds before looping

--- js/countdown.js
+++ js/countdown.js
@@ -1,37 +1,34 @@
-let countdownTimer;
-
-function startCountdown(countdownTime) {
-  clearInterval(countdownTimer);
-  
-  const now = Date.now();
-  const distance = countdownTime - now;
-  
-  if (distance <= 0) {
-    document.getElementById('countdown-timer').innerHTML = 'Block Created';
-    fetchCountdown();
-    return;
-  }
-  
-  const minutes = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  document.getElementById('countdown-timer').innerHTML = `${minutes}m ${seconds}s`;
-  
-  countdownTimer = setTimeout(() => {
-    startCountdown(countdownTime);
-  }, 1000);
-}
+let countdownInterval;
+let lastCountdownTime;
 
 function fetchCountdown() {
   fetch('https://nosostats.com:49443/api/endOfBlockInUnix')
     .then(response => response.text())
     .then(data => {
       const countdownTime = data.trim() * 1000;
-      startCountdown(countdownTime);
+      lastCountdownTime = countdownTime;
+      startCountdown();
     })
     .catch(error => {
       console.error('Error:', error);
     });
 }
 
-// Call fetchCountdown to start the countdown
+function startCountdown() {
+  clearInterval(countdownInterval);
+  countdownInterval = setInterval(() => {
+    const now = Date.now();
+    const distance = lastCountdownTime - now;
+    const minutes = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById('countdown-timer').innerHTML = `${minutes}m ${seconds}s`;
+    if (distance < 0) {
+      clearInterval(countdownInterval);
+      document.getElementById('countdown-timer').innerHTML = 'Block Created';
+      setTimeout(fetchCountdown, 5000);
+      setTimeout(startCountdown, 5000);
+    }
+  }, 1000);
+}
+
 fetchCountdown();
