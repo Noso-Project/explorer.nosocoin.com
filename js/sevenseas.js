29c89d77296624bb5e719e05d6e81c9180272901
gonefishing4422 <119458070+gonefishing4422@users.noreply.github.com>
2023-04-10 01:35:12 -0500

    update market

--- js/sevenseas.js
+++ js/sevenseas.js
@@ -1,60 +1,36 @@
-    // Define API URL
-    const apiUrl = 'https://www.sevenseas.exchange/api/v1/history/NOSO-USDT';
+          function fetchMarketData() {
+        // NOSO-USDT
+        const usdtApiUrl = 'https://www.sevenseas.exchange/api/v1/markets/NOSO-USDT';
 
-    // Make API request
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            // Sort data by time
-            data.sort((a, b) => a.time - b.time);
+        fetch(usdtApiUrl)
+            .then(response => response.json())
+            .then(data => {
 
-            // Create chart options object
-            const options = {
-                chart: {
-                    type: 'candlestick',
-                    height: 500,
-                    backgroundColor: '#222222'
-                },
-                title: {
-                    text: 'P2P trades | NOSO/USDT',
-                    style: {
-                        color: '#fbe04d'
-                    }
-                },
-                xAxis: {
-                    type: 'datetime',
-                    labels: {
-                        style: {
-                            color: 'white'
-                        }
-                    }
-                },
-                yAxis: {
-                    title: {
-                        text: 'Price',
-                        style: {
-                            color: 'white'
-                        }
-                    },
-                    labels: {
-                        style: {
-                            color: 'white'
-                        }
-                    }
-                },
-                series: [{
-                    name: 'Price',
-                    data: data.map(d => [d.time, d.price, d.price, d.price, d.price]),
-                    upColor: 'green',
-                    color: 'red',
-                    lineColor: 'yellow',
-                    tooltip: {
-                        valueDecimals: 2
-                    }
-                }]
-            };
+                const nosoUsdtLastPrice = data.lastPrice;
+                const nosoUsdtTradeVolume = data.volume;
 
-            // Render chart
-            Highcharts.stockChart('sevenseas-container', options);
-        })
-        .catch(error => console.error(error));
+
+                document.getElementById('noso-usdt-lastPrice').innerText = ` ${nosoUsdtLastPrice}`;
+                document.getElementById('noso-usdt-trade-volume').innerText = `NOSO-USDT Trade Volume: ${nosoUsdtTradeVolume}`;
+            })
+            .catch(error => console.error(error));
+
+        // NOSO-BTC
+        const btcApiUrl = 'https://www.sevenseas.exchange/api/v1/markets/NOSO-BTC';
+
+        fetch(btcApiUrl)
+            .then(response => response.json())
+            .then(data => {
+
+                const nosoBtcLastPrice = data.lastPrice;
+                const nosoBtcTradeVolume = data.volume;
+
+
+                document.getElementById('noso-btc-lastPrice').innerText = `${nosoBtcLastPrice}`;
+                document.getElementById('noso-btc-trade-volume').innerText = `NOSO-BTC Trade Volume: ${nosoBtcTradeVolume}`;
+            })
+            .catch(error => console.error(error));
+                  }
+
+        fetchMarketData();
+        setInterval(fetchMarketData, 60000);
