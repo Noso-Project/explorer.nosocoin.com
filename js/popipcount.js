664ba11c85fe448dce9d3f21063d5fc9b319e2e3
gonefishing4422 <119458070+gonefishing4422@users.noreply.github.com>
2023-04-25 22:12:10 -0500

    update popw count tile

--- js/popipcount.js
+++ js/popipcount.js
@@ -1,15 +1,19 @@
-      fetch("https://nosostats.com:49443/api/poolStatsV2")
-        .then((response) => response.json())
-        .then((data) => {
-          const numPools = data.length;
-          let totalMiners = 0;
-          for (let i = 0; i < numPools; i++) {
-            totalMiners += parseInt(data[i].Miners);
-          }
-          const avgMiners = Math.floor(totalMiners / numPools);
-          document.getElementById("popipcount").textContent =
-            "" + avgMiners;
-        })
-        .catch((error) => {
-          console.error("Error fetching pool stats:", error);
-        });
+fetch("https://nosostats.com:49443/api/poolStatsV2")
+  .then((response) => response.json())
+  .then((data) => {
+    const numPools = data.length;
+    let totalMiners = 0;
+    let numValidPools = 0;
+    for (let i = 0; i < numPools; i++) {
+      const poolMiners = parseInt(data[i].Miners);
+      if (poolMiners > 0) {
+        totalMiners += poolMiners;
+        numValidPools++;
+      }
+    }
+    const avgMiners = numValidPools > 0 ? Math.floor(totalMiners / numValidPools) : 0;
+    document.getElementById("popipcount").textContent = "" + avgMiners;
+  })
+  .catch((error) => {
+    console.error("Error fetching pool stats:", error);
+  });
