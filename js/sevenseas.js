7558adcfc3da5c11c946e0cff08ae46008511d6e
gonefishing4422 <119458070+gonefishing4422@users.noreply.github.com>
2023-06-12 18:12:29 -0500

    update-usdt-to-txbit

--- js/sevenseas.js
+++ js/sevenseas.js
@@ -1,36 +1,30 @@
-          function fetchMarketData() {
-        // NOSO-USDT
-        const usdtApiUrl = 'https://www.sevenseas.exchange/api/v1/markets/NOSO-USDT';
+function fetchMarketData() {
+  // NOSO-USDT
+  const usdtApiUrl = 'http://api.nosocoin.com:3001/api/public/getticker?market=NOSO/USDT';
 
-        fetch(usdtApiUrl)
-            .then(response => response.json())
-            .then(data => {
+  fetch(usdtApiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const nosoUsdtLastPrice = data.result.Last;
 
-                const nosoUsdtLastPrice = data.lastPrice;
-                const nosoUsdtTradeVolume = data.volume;
+      document.getElementById('noso-usdt-lastPrice').innerText = ` ${nosoUsdtLastPrice}`;
+    })
+    .catch(error => console.error(error));
 
+  // NOSO-BTC
+  const btcApiUrl = 'https://www.sevenseas.exchange/api/v1/markets/NOSO-BTC';
 
-                document.getElementById('noso-usdt-lastPrice').innerText = ` ${nosoUsdtLastPrice}`;
-                document.getElementById('noso-usdt-trade-volume').innerText = `NOSO-USDT Trade Volume: ${nosoUsdtTradeVolume}`;
-            })
-            .catch(error => console.error(error));
+  fetch(btcApiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const nosoBtcLastPrice = data.lastPrice;
+      const nosoBtcTradeVolume = data.volume;
 
-        // NOSO-BTC
-        const btcApiUrl = 'https://www.sevenseas.exchange/api/v1/markets/NOSO-BTC';
+      document.getElementById('noso-btc-lastPrice').innerText = `${nosoBtcLastPrice}`;
+      document.getElementById('noso-btc-trade-volume').innerText = `NOSO-BTC Trade Volume: ${nosoBtcTradeVolume}`;
+    })
+    .catch(error => console.error(error));
+}
 
-        fetch(btcApiUrl)
-            .then(response => response.json())
-            .then(data => {
-
-                const nosoBtcLastPrice = data.lastPrice;
-                const nosoBtcTradeVolume = data.volume;
-
-
-                document.getElementById('noso-btc-lastPrice').innerText = `${nosoBtcLastPrice}`;
-                document.getElementById('noso-btc-trade-volume').innerText = `NOSO-BTC Trade Volume: ${nosoBtcTradeVolume}`;
-            })
-            .catch(error => console.error(error));
-                  }
-
-        fetchMarketData();
-        setInterval(fetchMarketData, 60000);
+fetchMarketData();
+setInterval(fetchMarketData, 60000);
