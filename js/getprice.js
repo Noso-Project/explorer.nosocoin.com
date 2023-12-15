ea7fc6d877904ff8264bd6ad03daf8e46860977d
gonefishing4422 <119458070+gonefishing4422@users.noreply.github.com>
2023-12-14 21:36:58 -0600

    Update to use price api

--- js/getprice.js
+++ js/getprice.js
@@ -1,26 +1,15 @@
-async function fetchLivecoinWatchData() {
+async function fetchNosoCoinData() {
   try {
-    const response = await fetch(new Request("https://api.livecoinwatch.com/coins/single"), {
-      method: "POST",
-      headers: new Headers({
-        "content-type": "application/json",
-        "x-api-key": "20e72753-31e6-46f4-8ed0-a8e92161b1fc", // Replace with your actual API key
-      }),
-      body: JSON.stringify({
-        currency: "USD",
-        code: "NOSO",
-        meta: false,
-      }),
-    });
+    const response = await fetch("https://api.nosocoin.com/info/price?range=day&interval=1");
 
     if (response.ok) {
       const result = await response.json();
       return result;
     } else {
-      throw new Error("LivecoinWatch API request failed");
+      throw new Error("NosoCoin API request failed");
     }
   } catch (error) {
-    console.error("Error fetching LivecoinWatch data:", error);
+    console.error("Error fetching NosoCoin data:", error);
     return null;
   }
 }
@@ -45,21 +34,22 @@ async function displayData() {
   const nosoUsdtLastPriceElement = document.getElementById("noso-usdt-lastPrice");
   const nosoBtcLastPriceElement = document.getElementById("noso-btc-lastPrice");
 
-  const livecoinWatchData = await fetchLivecoinWatchData();
-  if (livecoinWatchData) {
-    const nosoUsdtLastPrice = livecoinWatchData.rate.toFixed(2); // Round to two decimal places
+  const nosoCoinData = await fetchNosoCoinData();
+  if (nosoCoinData && nosoCoinData.length > 0) {
+    const lastDataPoint = nosoCoinData[nosoCoinData.length - 1];
+    const nosoUsdtLastPrice = lastDataPoint.price.toFixed(2); // Round to two decimal places
     nosoUsdtLastPriceElement.textContent = nosoUsdtLastPrice;
 
     const btcValue = await fetchBTCValue();
     if (btcValue) {
-      const nosoValueInUsdt = livecoinWatchData.rate; // Use the obtained Noso value
+      const nosoValueInUsdt = lastDataPoint.price; // Use the obtained Noso value
       const nosoToBtcValue = (nosoValueInUsdt / btcValue).toFixed(8); // Round to eight decimal places
       nosoBtcLastPriceElement.textContent = nosoToBtcValue;
     } else {
       nosoBtcLastPriceElement.textContent = "Failed to calculate Noso to BTC value.";
     }
   } else {
-    nosoUsdtLastPriceElement.textContent = "Failed to fetch data from LivecoinWatch API.";
+    nosoUsdtLastPriceElement.textContent = "Failed to fetch data from NosoCoin API.";
     nosoBtcLastPriceElement.textContent = "";
   }
 }
