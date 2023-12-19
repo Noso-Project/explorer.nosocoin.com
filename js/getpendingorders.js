00f6c6f4a5e4b464146fdb6044bdbf35c52efd58
gonefishing4422 <119458070+gonefishing4422@users.noreply.github.com>
2023-12-18 22:57:35 -0600

    Update_pendingorders

--- js/getpendingorders.js
+++ js/getpendingorders.js
@@ -1,40 +1,72 @@
-    function refreshTable() {
-      fetch('https://api.nosostats.com:8078', {
-        method: 'POST',
-        headers: {
-          'Origin': 'https://api.nosostats.com'
-        },
-        body: JSON.stringify({
-          "jsonrpc": "2.0",
-          "method": "getpendingorders",
-          "params": [],
-          "id": 14
-        })
-      })
-      .then(response => response.json())
-      .then(data => {
-        const pendings = data.result[0].pendings;
-        const tableBody = document.getElementById('getpendingorders');
-        tableBody.innerHTML = ''; // clear existing rows
-        pendings.slice(0, 5).forEach(pending => {
-          const [orderType, sender, receiver, orderAmount, orderFee] = pending.split(',');
-          const senderLink = `<a href="getaddressbalance.html?address=${sender}">${sender}</a>`;
-          const receiverLink = `<a href="getaddressbalance.html?address=${receiver}">${receiver}</a>`;
-	const orderAmountFormatted = (orderAmount * 0.00000001).toFixed(8);
-	const orderFeeFormatted = (orderFee * 0.00000001).toFixed(8);
-	
-	const row = `
-	  <tr>
-	    <td>&nbsp;&nbsp;&nbsp;&nbsp;<img src="img/logo_clearbg.png" width="15px">&nbsp;${orderType}</td>
-	    <td colspan="3"><span>Sender&nbsp;&nbsp;${senderLink}<br>Receiver&nbsp;&nbsp;${receiverLink}</span></td>
-	    <td><span><b>Sent</b>&nbsp;&nbsp;${orderAmountFormatted}<br><b>Fee</b>&nbsp;&nbsp;${orderFeeFormatted}</span></td>
-	  </tr>
-	`;
-          tableBody.insertAdjacentHTML('beforeend', row);
-        });
-      })
-      .catch(error => console.error(error));
-    }
+function refreshTable() {
+  fetch('https://api.nosostats.com:8078', {
+    method: 'POST',
+    headers: {
+      'Origin': 'https://api.nosostats.com'
+    },
+    body: JSON.stringify({
+      "jsonrpc": "2.0",
+      "method": "getpendingorders",
+      "params": [],
+      "id": 14
+    })
+  })
+  .then(response => response.json())
+  .then(data => {
+    const pendings = data.result[0].pendings;
+    const container = document.getElementById('getpendingorders');
+    container.innerHTML = ''; // clear existing content
+    pendings.forEach(pending => {
+      const [orderID, orderTimestamp, orderType, sender, receiver, orderAmount, orderFee, orderReference] = pending.split(',');
+      const senderLink = `<a href="getaddressbalance.html?address=${sender}" style="color: #428bca;">${sender}</a>`;
+      const receiverLink = `<a href="getaddressbalance.html?address=${receiver}" style="color: #428bca;">${receiver}</a>`;
+      const orderAmountFormatted = (orderAmount * 0.00000001).toFixed(8);
+      const orderFeeFormatted = (orderFee * 0.00000001).toFixed(8);
 
-    refreshTable();
-    setInterval(refreshTable, 20000); // refresh every 20 seconds
+      const orderIDLink = `<a href="getordersinfo.html?orderid=${orderID}" style="color: #428bca;">${orderID}</a>`;
+      const minutesAgo = timeAgo(orderTimestamp);
+      const timestampDisplay = `<span style="color: lightgray;"> ${minutesAgo} minute(s) ago</span>`;
+
+      const divContainer = document.createElement('div');
+      divContainer.classList.add('order-container');
+
+      const divLeft = document.createElement('div');
+      divLeft.classList.add('left-column');
+      divLeft.innerHTML = `
+        <br><img src="img/logo_clearbg.png" width="15px">&nbsp;<span style="color: lightgray;">${orderType}</span><br>
+        ${timestampDisplay}
+      `;
+      divContainer.appendChild(divLeft);
+
+      const divRight = document.createElement('div');
+      divRight.classList.add('right-column');
+      divRight.innerHTML = `
+        <span style="color: lightgray;">OrderID:&nbsp;&nbsp;${orderIDLink}</span>
+        <span style="color: lightgray;">Amount:&nbsp;&nbsp;<b>${orderAmountFormatted}</b></span>
+        <span style="color: lightgray;">TxFee:&nbsp;&nbsp;<b>${orderFeeFormatted}</b></span>
+        <span style="color: lightgray;">Sender:&nbsp;&nbsp;${senderLink}</span>
+        <span style="color: lightgray;">Receiver:&nbsp;&nbsp;${receiverLink}</span>
+        <span style="color: lightgray;">Reference:&nbsp;&nbsp;${orderReference}</span><br>        
+        <hr style="border: 1px solid #222222; width: 420px;" >
+      `;
+      divContainer.appendChild(divRight);
+
+      container.appendChild(divContainer);
+    });
+  })
+  .catch(error => console.error(error));
+}
+
+// Function to convert timestamp to "minutes ago"
+function timeAgo(timestamp) {
+  const currentDate = new Date();
+  const orderDate = new Date(timestamp * 1000); // Assuming timestamp is in seconds
+
+  const timeDifference = currentDate - orderDate;
+  const minutesAgo = Math.floor(timeDifference / (1000 * 60));
+
+  return minutesAgo;
+}
+
+refreshTable();
+setInterval(refreshTable, 20000); // refresh every 20 seconds
--- js/getpendingorders2.js
+++ js/getpendingorders2.js
@@ -1,41 +1,58 @@
-    function refreshTable() {
-      fetch('https://api.nosostats.com:8078', {
-        method: 'POST',
-        headers: {
-          'Origin': 'https://api.nosostats.com'
-        },
-        body: JSON.stringify({
-          "jsonrpc": "2.0",
-          "method": "getpendingorders",
-          "params": [],
-          "id": 14
-        })
-      })
-      .then(response => response.json())
-      .then(data => {
-        const pendings = data.result[0].pendings;
-        const tableBody = document.getElementById('getpendingorders');
-        tableBody.innerHTML = ''; // clear existing rows
-        pendings.forEach(pending => {
-          const [orderType, sender, receiver, orderAmount, orderFee] = pending.split(',');
-          const senderLink = `<a href="getaddressbalance.html?address=${sender}">${sender}</a>`;
-          const receiverLink = `<a href="getaddressbalance.html?address=${receiver}">${receiver}</a>`;
-	const orderAmountFormatted = (orderAmount * 0.00000001).toFixed(8);
-	const orderFeeFormatted = (orderFee * 0.00000001).toFixed(8);
-	
-	const row = `
-	  <tr>
-	    <td width="80px">&nbsp;&nbsp;&nbsp;&nbsp;<img src="img/logo_clearbg.png" width="15px">&nbsp;${orderType}</td>
-	    <td width="300px"><span>&nbsp;&nbsp;&nbsp;NOSO sent:&nbsp;&nbsp;<font color="gray">${orderAmountFormatted}</font><b>&nbsp;&nbsp;&nbsp;Tx Fee:</b>&nbsp;&nbsp;<font color="gray">${orderFeeFormatted}</font></span></td>
-	    <td width="300px"><span>Sender:&nbsp;&nbsp;${senderLink}</span></td>
-	    <td><span>Receiver:&nbsp;&nbsp;${receiverLink}</span></td>
-	  </tr>
-	`;
-          tableBody.insertAdjacentHTML('beforeend', row);
-        });
-      })
-      .catch(error => console.error(error));
-    }
+function refreshTable() {
+  fetch('https://api.nosostats.com:8078', {
+    method: 'POST',
+    headers: {
+      'Origin': 'https://api.nosostats.com'
+    },
+    body: JSON.stringify({
+      "jsonrpc": "2.0",
+      "method": "getpendingorders",
+      "params": [],
+      "id": 14
+    })
+  })
+  .then(response => response.json())
+  .then(data => {
+    const pendings = data.result[0].pendings;
+    const tableBody = document.getElementById('getpendingorders');
+    tableBody.innerHTML = ''; // clear existing rows
+    pendings.forEach(pending => {
+      const [orderID, orderTimestamp, orderType, sender, receiver, orderAmount, orderFee, orderReference] = pending.split(',');
+      const senderLink = `<a href="getaddressbalance.html?address=${sender}">${sender}</a>`;
+      const receiverLink = `<a href="getaddressbalance.html?address=${receiver}">${receiver}</a>`;
+      const orderAmountFormatted = (orderAmount * 0.00000001).toFixed(8);
+      const orderFeeFormatted = (orderFee * 0.00000001).toFixed(8);
+      const orderIDLink = `<a href="getordersinfo.html?orderid=${orderID}" style="color: #428bca;">${orderID}</a>`;
+      const minutesAgo = timeAgo(orderTimestamp);
 
-    refreshTable();
-    setInterval(refreshTable, 20000); // refresh every 20 seconds
+      const row = `
+        <tr>
+          <td width="300px"><img src="img/logo_clearbg.png" width="15px">&nbsp;${orderType}<span><br>
+          <span>Order ID:&nbsp;&nbsp;${orderIDLink}</span><br>
+          <span>Timestamp:&nbsp;&nbsp;${minutesAgo} ago</span><br>
+          <span>NOSO sent:&nbsp;&nbsp;<font color="gray">${orderAmountFormatted}</font><b>&nbsp;&nbsp;&nbsp;Tx Fee:</b>&nbsp;&nbsp;<font color="gray">${orderFeeFormatted}</font></span><br>
+          <span>Sender:&nbsp;&nbsp;${senderLink}</span><br>
+          <span>Receiver:&nbsp;&nbsp;${receiverLink}</span><br>
+          <span>Order Reference:&nbsp;&nbsp;${orderReference}</span>
+          </td>
+        </tr>
+      `;
+      tableBody.insertAdjacentHTML('beforeend', row);
+    });
+  })
+  .catch(error => console.error(error));
+}
+
+// Function to convert timestamp to "minutes ago"
+function timeAgo(timestamp) {
+  const currentDate = new Date();
+  const orderDate = new Date(timestamp * 1000); // Assuming timestamp is in seconds
+
+  const timeDifference = currentDate - orderDate;
+  const minutesAgo = Math.floor(timeDifference / (1000 * 60));
+
+  return minutesAgo === 0 ? 'just now' : `${minutesAgo} minutes`;
+}
+
+refreshTable();
+setInterval(refreshTable, 20000); // refresh every 20 seconds
