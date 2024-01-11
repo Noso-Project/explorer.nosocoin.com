87a3dafd27239e0cca18d1799d59e63c27f90af0
gonefishing4422 <119458070+gonefishing4422@users.noreply.github.com>
2024-01-10 18:43:17 -0600

    Add content to masternodes page

--- js/getblockmns.js
+++ js/getblockmns.js
@@ -1,3 +1,60 @@
+async function fetchNosoCoinData() {
+  try {
+    const response = await fetch("https://api.nosocoin.com/info/price?range=day&interval=1");
+
+    if (response.ok) {
+      const result = await response.json();
+      return result;
+    } else {
+      throw new Error("NosoCoin API request failed");
+    }
+  } catch (error) {
+    console.error("Error fetching NosoCoin data:", error);
+    return null;
+  }
+}
+
+async function fetchBTCValue() {
+  try {
+    const response = await fetch('https://tradeogre.com/api/v1/markets');
+    if (response.ok) {
+      const data = await response.json();
+      const btcUsdtAsk = data.find(market => market.hasOwnProperty("BTC-USDT"))["BTC-USDT"]["ask"];
+      return btcUsdtAsk;
+    } else {
+      throw new Error("BTC value fetch request failed");
+    }
+  } catch (error) {
+    console.error("Error fetching BTC value:", error);
+    return null;
+  }
+}
+
+async function displayData() {
+  const nosoUsdtLastPriceElement = document.getElementById("noso-usdt-lastPrice");
+  const nosoBtcLastPriceElement = document.getElementById("noso-btc-lastPrice");
+
+  const nosoCoinData = await fetchNosoCoinData();
+  if (nosoCoinData && nosoCoinData.length > 0) {
+    const lastDataPoint = nosoCoinData[nosoCoinData.length - 1];
+    const nosoUsdtLastPrice = lastDataPoint.price.toFixed(2); // Round to two decimal places
+    nosoUsdtLastPriceElement.textContent = nosoUsdtLastPrice;
+
+    const btcValue = await fetchBTCValue();
+    if (btcValue) {
+      const nosoValueInUsdt = lastDataPoint.price; // Use the obtained Noso value
+      const nosoToBtcValue = (nosoValueInUsdt / btcValue).toFixed(8); // Round to eight decimal places
+      nosoBtcLastPriceElement.textContent = nosoToBtcValue;
+    } else {
+      nosoBtcLastPriceElement.textContent = "Failed to calculate Noso to BTC value.";
+    }
+  } else {
+    nosoUsdtLastPriceElement.textContent = "API Unavailable.";
+    nosoBtcLastPriceElement.textContent = "";
+  }
+}
+
+displayData();
 // Function to make the API call and update the DOM for mn-lock-funds
 async function fetchLockFunds() {
   try {
@@ -116,9 +173,10 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById('total-reward').textContent = (data.result[0].total * 0.00000001).toFixed(8);
         document.getElementById('node-count').textContent = data.result[0].count;
         document.getElementById('node-reward').textContent = (data.result[0].reward * 0.00000001).toFixed(8);
-        document.getElementById('node-24hr-reward').textContent = (data.result[0].reward * 0.00000001 * 144).toFixed(8);
-        document.getElementById('node-7day-reward').textContent = (data.result[0].reward * 0.00000001 * 1008).toFixed(8);
-        document.getElementById('node-30day-reward').textContent = (data.result[0].reward * 0.00000001 * 4320).toFixed(8);
+        document.getElementById('node-24hr-reward').textContent = (data.result[0].reward * 0.00000001 * 144).toFixed(0);
+        document.getElementById('node-7day-reward').textContent = (data.result[0].reward * 0.00000001 * 1008).toFixed(0);
+        document.getElementById('node-30day-reward').textContent = (data.result[0].reward * 0.00000001 * 4320).toFixed(0);
+        document.getElementById('node-365day-reward').textContent = (data.result[0].reward * 0.00000001 * 52560).toFixed(0);        
 
         // Blockheight
         var blockheightElement = document.getElementById('blockheight');
@@ -143,9 +201,36 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // Call the function to fetch data for the line chart
         fetchBlockDataForChart();
+  // Calculate node rewards in USDT and BTC
+  const nosoUsdtLastPriceElement = document.getElementById('noso-usdt-lastPrice');
+  const nosoBtcLastPriceElement = document.getElementById('noso-btc-lastPrice');
+
+  if (nosoUsdtLastPriceElement && nosoBtcLastPriceElement) {
+    const nosoUsdtLastPrice = parseFloat(nosoUsdtLastPriceElement.textContent);
+    const nosoBtcLastPrice = parseFloat(nosoBtcLastPriceElement.textContent);
+
+    const reward = data.result[0].reward * 0.00000001;
+
+    document.getElementById('node-24hr-reward-usdt').textContent = (reward * 144 * nosoUsdtLastPrice).toFixed(2);
+    document.getElementById('node-7day-reward-usdt').textContent = (reward * 1008 * nosoUsdtLastPrice).toFixed(2);
+    document.getElementById('node-30day-reward-usdt').textContent = (reward * 4320 * nosoUsdtLastPrice).toFixed(2);
+    document.getElementById('node-365day-reward-usdt').textContent = (reward * 52560 * nosoUsdtLastPrice).toFixed(2);
+    document.getElementById('node-value-usdt').textContent = (10500 * nosoUsdtLastPrice).toFixed(2);
+
+    document.getElementById('node-24hr-reward-btc').textContent = (reward * 144 * nosoBtcLastPrice).toFixed(8);
+    document.getElementById('node-7day-reward-btc').textContent = (reward * 1008 * nosoBtcLastPrice).toFixed(8);
+    document.getElementById('node-30day-reward-btc').textContent = (reward * 4320 * nosoBtcLastPrice).toFixed(8);
+    document.getElementById('node-365day-reward-btc').textContent = (reward * 52560 * nosoBtcLastPrice).toFixed(8);
+    document.getElementById('node-value-btc').textContent = (10500 * nosoBtcLastPrice).toFixed(8);   
+  } else {
+    console.error("Elements 'noso-usdt-lastPrice' or 'noso-btc-lastPrice' not found.");
+  }      
       })
       .catch(error => console.error(error));
+  
   }
+
+
 
   // Fetch the currentHeight if blockHeight is not available
   if (!blockHeight) {
